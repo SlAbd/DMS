ae4cb8f1e88f103b1732823cdf8b6f304419981d
Abdelhadi Slimane <sliwabdel15@gmail.com>
2025-03-28 14:47:48 +0000

    project structure

---
+++ src/main/java/com/project/dms/DMS.java
@@ -0,0 +1,13 @@
+package com.project.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMS {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DMS.class, args);
+    }
+
+}
